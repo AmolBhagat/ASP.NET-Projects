36559e4bbf88a4dea1d5d6ab1a76b42e406b4c4d
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-10 15:25:42 +0530

    First Commit

---
+++ Diffrn betwn view session and application state/Diffrn betwn view session and application state/ViewState2.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Diffrn_betwn_view_session_and_application_state
+{
+    public partial class ViewState2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (ViewState["Clicks"] == null)
+                {
+                    ViewState["Clicks"] = 0;
+                }
+                TextBox1.Text = ViewState["Clicks"].ToString();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int ClicksCount = (int)ViewState["Clicks"] + 1;
+            TextBox1.Text = ClicksCount.ToString();
+            ViewState["Clicks"] = ClicksCount;
+        }
+    }
+}
