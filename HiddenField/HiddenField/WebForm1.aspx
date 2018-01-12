330a257675ff9263edd1d00f43422a1de666ffe1
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-12 09:15:07 +0530

    First Commit

---
+++ HiddenField/HiddenField/WebForm1.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="HiddenField.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hiddenfield Demo</title>
+    <script lang="javascript" type="text/javascript">
+
+        function GetHiddenFieldValue()
+        {
+            alert('EmployeeID =' + document.getElementById('HiddenField1').value);
+        }
+
+
+        function Button1_onclick()
+        {
+            GetHiddenFieldValue();
+        }
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:HiddenField ID="HiddenField1" runat="server" />
+        <br />
+        <table>
+            <tr>
+                <td>Name</td><td><asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+            </tr>
+
+             <tr>
+                <td>Gender</td><td><asp:TextBox ID="txtGender" runat="server"></asp:TextBox></td>
+            </tr>
+
+             <tr>
+                <td>Department</td><td><asp:TextBox ID="txtDepartment" runat="server"></asp:TextBox></td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+
+                </td>
+
+                <td>
+                    <asp:Button ID="btnLoad" runat="server" Text="Load Employee" OnClick="btnLoad_Click" Width="128px" />
+                </td>
+               <td>
+                    <input id="Button1" type="button" value="button" />
+               </td>
+                   
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ HiddenField/HiddenField/WebForm1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace HiddenField
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadEmployee();
+            }
+           
+        }
+
+        private void LoadEmployee()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                string SqlQuery = "select ID, Name,Gender,Department from tblEmployees where ID=2";
+                SqlCommand cmd = new SqlCommand(SqlQuery, con);
+                con.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while(dr.Read())
+                    {
+                        txtName.Text = dr["Name"].ToString();
+                        txtGender.Text = dr["Gender"].ToString();
+                        txtDepartment.Text = dr["Department"].ToString();
+                        HiddenField1.Value = dr["ID"].ToString();
+
+                    }
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                string SqlQuery = "Update tblEmployees set Name=@Name,Gender=@Gender,Department=@Department where ID=@ID";
+                using (SqlCommand cmd = new SqlCommand(SqlQuery, con))
+                {
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
+                    cmd.Parameters.AddWithValue("@Department", txtDepartment.Text);
+                    cmd.Parameters.AddWithValue("@ID", HiddenField1.Value);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+
+                    txtName.Text = "";
+                    txtGender.Text = "";
+                    txtDepartment.Text = "";
+                }
+
+            }
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadEmployee();
+        }
+    }
+}
