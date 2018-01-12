b96b885ae432f1d96f31b383212472b9fbc89b64
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-12 14:41:26 +0530

    First Commit

---
+++ DropdownList in ASP.NET/DropdownList in ASP.NET/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DropdownList_in_ASP.NET.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            
+        </asp:DropDownList>
+        <br />
+        <br />
+        <br />
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" Height="23px" Text="Button" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ DropdownList in ASP.NET/DropdownList in ASP.NET/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropdownList_in_ASP.NET
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ListItem maleListItem = new ListItem("Male", "1");
+                ListItem FemaleListItem = new ListItem("Female", "2");
+
+                DropDownList1.Items.Add(maleListItem);
+                DropDownList1.Items.Add(FemaleListItem);
+            }
+        }
+    }
+}
