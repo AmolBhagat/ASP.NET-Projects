a1e576f304c520a82f8e6dbe166d39497d98e721
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-12 15:23:08 +0530

    First Commit

---
+++ Binding Data from database to Dropdownlist/Binding Data from database to Dropdownlist/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Binding_Data_from_database_to_Dropdownlist.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <label>City</label>
+        <asp:DropDownList ID="ddlCity" runat="server"></asp:DropDownList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Binding Data from database to Dropdownlist/Binding Data from database to Dropdownlist/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace Binding_Data_from_database_to_Dropdownlist
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if(!IsPostBack)
+            {
+                string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlCommand cmd = new SqlCommand("Select CityId, CityName, Country from tblCity", con);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    ddlCity.DataTextField = "CityName";
+                    ddlCity.DataValueField = "CityId";
+                    ddlCity.DataSource = rdr;
+                    ddlCity.DataBind();
+                }
+            }
+        }
+    }
+}
